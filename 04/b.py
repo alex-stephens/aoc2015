1fe53ddb32341d8c790415c5c11f2649cf09d56c
Alex <alexstephens9@gmail.com>
2020-12-26 15:48:10 +0000

    Day 4

---
+++ 04/b.py
@@ -0,0 +1,16 @@
+import hashlib 
+key = open('input.txt').readline()
+
+def check(key, num):
+    string = key + str(num)
+    hash = hashlib.md5(string.encode())
+    result = hash.hexdigest()
+    nzeros = 6
+    return result.startswith('0'*nzeros)
+
+n = 0
+while True:
+    if check(key, n):
+        break
+    n += 1
+print(n)

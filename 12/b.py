5284c050cd043b58cf131533688bb62f1d34b33e
Alex <alexstephens9@gmail.com>
2020-12-27 11:55:04 +0000

    Day 12

---
+++ 12/b.py
@@ -0,0 +1,16 @@
+import json
+
+def recursive_sum(s):
+    val = 0
+    if type(s) == int:
+        val += s
+    elif type(s) == dict:
+        if 'red' not in s.values():
+            val += sum([recursive_sum(s[k]) for k in s.keys()])
+    elif type(s) == list:
+        val += sum([recursive_sum(x) for x in s])
+    return val
+
+s = open('input.txt').readline()
+s = json.loads(s)
+print(recursive_sum(s))
